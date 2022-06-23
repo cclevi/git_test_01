dcaca7aff299dddf623c613e0bdf39cce07f6d00
cclevi <cclevi@atguigu.com>
2022-06-23 16:41:55 +0800

    second commit

--- src/main/java/com/chen/main/demo1.java
+++ src/main/java/com/chen/main/demo1.java
@@ -3,5 +3,6 @@ package com.chen.main;
 public class demo1 {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("fall in love");
     }
 }
